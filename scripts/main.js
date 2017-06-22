ffb9e29e6c23bebf3782efd6ae54ce7759f41190
Javier Steinaker <jsteinaker@gmail.com>
2017-06-22 10:49:10 +0200

    First take on reading from Firebase

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Referencia a la tabla y a la base de datos
+var table = document.getElementsByTagName('table')[0];
+var database = firebase.database().ref('gear');
+
+// Iniciamos
+database.on("child_added", function(child) {
+	console.log(child.key+': '+child.val());
+	var tr = document.createElement('tr');
+	var td = document.createElement('td');
+	td.innerText = child.val();
+	tr.appendChild(td);
+	table.appendChild(tr);
+});
